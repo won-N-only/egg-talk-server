e3156078cf98427ac05c9d58b07d6c1bef0e3e6a
sh940701 <visioner2168@gmail.com>
2024-06-18 20:16:19 +0900

    feat: add chats skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,13 @@ import { AuthModule } from './auth/auth.module';
 import { AwsController } from './aws/aws.controller';
 import { AwsService } from './aws/aws.service';
 import { AwsModule } from './aws/aws.module';
+import { ChatsController } from './chats/chats.controller';
+import { ChatsService } from './chats/chats.service';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, AwsModule],
-  controllers: [AppController, UsersController, AuthController, AwsController],
-  providers: [AppService, UsersService, AuthService, AwsService],
+  imports: [UsersModule, AuthModule, AwsModule, ChatsModule],
+  controllers: [AppController, UsersController, AuthController, AwsController, ChatsController],
+  providers: [AppService, UsersService, AuthService, AwsService, ChatsService],
 })
 export class AppModule {}
