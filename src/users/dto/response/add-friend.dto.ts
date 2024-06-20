d466ffaf6343323108dd00d378cef8022e1223cc
won-n-only <siliver12@gmail.com>
2024-06-20 16:30:33 +0900

    update: add.friend.dto for POST /users/friends/:id response

---
+++ src/users/dto/response/add-friend.dto.ts
@@ -0,0 +1,19 @@
+export class ResAddFriendDto {
+  _id: string
+
+  id: string
+  nickname: string
+
+  gender: 'MALE' | 'FEMALE'
+  avatar: object
+
+  newNotification: boolean
+  notifications: string[] // Notification의 ObjectId 목록
+  friends: Array<{
+    _id: string // 친구 id
+    chatRoomId: string // 채팅방 id
+    newMessage: boolean
+  }>
+
+  createdAt: Date
+}
