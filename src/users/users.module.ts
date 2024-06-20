dc73a34f248999b5ac811207ebc46bec229d32dd
won-n-only <siliver12@gmail.com>
2024-06-20 15:19:27 +0900

    update: connect UserSchema to User.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,16 @@ import { UsersService } from './users.service'
 import { UsersRepository } from './users.repository'
 import { JwtAuthRestGuard } from '../guards/jwt-auth.rest.guard'
 import { JwtService } from '@nestjs/jwt'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UserSchema, User } from 'src/entities/user.entity'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema, collection: 'Users' },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, JwtService, JwtAuthRestGuard],
 })
