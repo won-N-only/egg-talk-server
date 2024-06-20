46b1ac27aa16e8a66979f6f77e29917a4f347268
won-n-only <siliver12@gmail.com>
2024-06-20 20:47:16 +0900

    update: add-friend.dto

--- src/users/dto/request/add-friend.dto.ts
+++ src/users/dto/request/add-friend.dto.ts
@@ -3,5 +3,9 @@ import { IsString, IsNotEmpty } from 'class-validator'
 export class ReqAddFriendDto {
   @IsString()
   @IsNotEmpty()
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
   friendId: string //objid아니고 그냥 id로
 }
