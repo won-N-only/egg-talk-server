288953633c6f488a7dee811397f2e62ca377d240
won-n-only <siliver12@gmail.com>
2024-06-20 20:46:33 +0900

    update: chat-room.entity

---
+++ src/entities/chat-room.entity.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document, Types } from 'mongoose'
+export type ChatRoomDocument = ChatRoom & Document
+
+@Schema({ timestamps: true })
+export class ChatRoom {
+
+  @Prop({ type: [{ type: Types.ObjectId }] })
+  chats: Types.ObjectId[]
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date
+}
+
+export const ChatRoomSchema = SchemaFactory.createForClass(ChatRoom)
