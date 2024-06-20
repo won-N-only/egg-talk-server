38b06776e0977f8331f0460f682c1d0259a022c4
won-n-only <siliver12@gmail.com>
2024-06-20 16:30:53 +0900

    update: friend.dto for POST /users/friends/:id request
    class 이름 변경

--- src/users/dto/request/add-friend.dto.ts
+++ src/users/dto/request/add-friend.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, IsNotEmpty } from 'class-validator'
 
-export class AddFriendDto {
+export class ReqAddFriendDto {
   @IsString()
   @IsNotEmpty()
   friendId: string //objid아니고 그냥 id로
