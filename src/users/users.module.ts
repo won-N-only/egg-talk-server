2c16abeb4a7b1a0ad4ef8c582dd0b32ffdbf4214
won-n-only <siliver12@gmail.com>
2024-06-21 15:37:38 +0900

    remove: unuse module
    친구추가 기능 제거하면서 쓰지않게된 ChatRoom모듈 제거

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,16 +5,9 @@ import { UsersService } from './users.service'
 import { UsersRepository } from './users.repository'
 import { JwtAuthRestGuard } from '../guards/jwt-auth.rest.guard'
 import { JwtService } from '@nestjs/jwt'
-import { MongooseModule } from '@nestjs/mongoose'
-import { ChatRoomSchema, ChatRoom } from 'src/entities/chat-room.entity'
 
 @Module({
-  imports: [
-    AuthModule,
-    MongooseModule.forFeature([
-      { name: ChatRoom.name, schema: ChatRoomSchema, collection: 'ChatRooms' },
-    ]),
-  ],
+  imports: [AuthModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, JwtService, JwtAuthRestGuard],
 })
