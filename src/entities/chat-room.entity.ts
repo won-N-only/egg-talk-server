f75bccb9cbd405aa47a95bdbd96d330bb2a60a35
won-n-only <siliver12@gmail.com>
2024-06-21 14:10:25 +0900

    remove: unnecessary Property
    {timestamps: true}이므로 createdAt 삭제

--- src/entities/chat-room.entity.ts
+++ src/entities/chat-room.entity.ts
@@ -1,14 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import {  Types } from 'mongoose'
+import { Types } from 'mongoose'
 
 @Schema({ timestamps: true })
 export class ChatRoom {
-
   @Prop({ type: [{ type: Types.ObjectId }] })
   chats: Types.ObjectId[]
-
-  @Prop({ type: Date, default: Date.now })
-  createdAt: Date
 }
 
 export const ChatRoomSchema = SchemaFactory.createForClass(ChatRoom)
