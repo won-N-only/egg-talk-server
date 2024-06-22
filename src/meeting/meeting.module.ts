2da96dd96b9d8d698dab2891e95df7fa88e3c783
Amborsia <ghdrms1220@naver.com>
2024-06-22 14:07:24 +0900

    jwt 원복

--- src/meeting/meeting.module.ts
+++ src/meeting/meeting.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common'
 import { MeetingGateway } from './meeting.gateway'
 import { OpenViduService } from './meeting.service'
 import { MeetingRepository } from './meeting.repository'
-// import { JwtAuthWsGuard } from '../guards/jwt-auth.ws.guard'
-// import { JwtService } from '@nestjs/jwt'
+import { JwtAuthWsGuard } from '../guards/jwt-auth.ws.guard'
+import { JwtService } from '@nestjs/jwt'
 
 @Module({
   providers: [
     MeetingGateway,
     OpenViduService,
     MeetingRepository,
-    // JwtService,
-    // JwtAuthWsGuard,
+    JwtService,
+    JwtAuthWsGuard,
   ],
 })
 export class MeetingModule { }
