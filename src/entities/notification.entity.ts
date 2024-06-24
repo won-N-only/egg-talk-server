c242f1bb844eca040544619d0bd9b98203d69023
won-n-only <siliver12@gmail.com>
2024-06-24 17:39:40 +0900

    feat: notification entity

--- src/entities/notification.entity.ts
+++ src/entities/notification.entity.ts
@@ -1,4 +1,17 @@
-import { Schema } from '@nestjs/mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Types } from 'mongoose'
+
+export enum NotificationTypes {
+  FRIEND = 'FRIEND',
+  PARTY = 'PARTY',
+}
 
 @Schema({ timestamps: true })
-export class Notification {}
+export class Notification {
+  @Prop({ type: Types.ObjectId, required: true, ref: 'User' })
+  from: Types.ObjectId
+
+  @Prop({ type: String, enum: NotificationTypes })
+  notificationType: string
+}
+export const NotificationSchema = SchemaFactory.createForClass(Notification)
