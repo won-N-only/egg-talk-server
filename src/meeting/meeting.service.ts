c702e4f1a2533c136f65c3bd95b8c29d85d1a518
Amborsia <ghdrms1220@naver.com>
2024-06-25 16:13:19 +0900

    refactor: 2명으로 수정

--- src/meeting/meeting.service.ts
+++ src/meeting/meeting.service.ts
@@ -179,7 +179,7 @@ export class OpenViduService {
         participants.length,
       )
 
-      if (participants.length === 4) {
+      if (participants.length === 2) {
         await this.startVideoChatSession(sessionName)
         // 새로운 세션을 생성하고 반환
         const newSessionName = this.generateSessionName()
