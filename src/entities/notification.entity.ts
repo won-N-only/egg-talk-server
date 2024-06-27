6b8f1706ac89dd54e4bcbda27581f074a08972e0
won-n-only <siliver12@gmail.com>
2024-06-27 23:35:45 +0900

    remove: '/notification.entity' type 'party' #14

--- src/entities/notification.entity.ts
+++ src/entities/notification.entity.ts
@@ -3,15 +3,11 @@ import { Types } from 'mongoose'
 
 export enum NotificationTypes {
   FRIEND = 'FRIEND',
-  PARTY = 'PARTY',
 }
 
 @Schema({ timestamps: true })
 export class Notification {
   @Prop({ type: Types.ObjectId, required: true, ref: 'User' })
   from: Types.ObjectId
-
-  @Prop({ type: String, enum: NotificationTypes })
-  notificationType: string
 }
 export const NotificationSchema = SchemaFactory.createForClass(Notification)
