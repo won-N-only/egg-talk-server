ce8611ab61f3974556ba8855087650fc56da08bf
won-n-only <siliver12@gmail.com>
2024-06-28 21:06:29 +0900

    feat: add skeleton to '/common/repository' #45
    Co-authored-by: Bambamsong <Bambamsong@users.noreply.github.com>

--- src/common/common.repository.ts
+++ src/common/common.repository.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model, Types } from 'mongoose'
+import { ChatRoom } from 'src/entities/chat-room.entity'
+import { Chat } from 'src/entities/chat.entity'
+import { User } from 'src/entities/user.entity'
 
 @Injectable()
-export class CommonRepository {}
+export class CommonRepository {
+  constructor(
+    @InjectModel(ChatRoom.name) private chatRoomModel: Model<ChatRoom>,
+    @InjectModel(Chat.name) private chatModel: Model<Chat>,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+}
