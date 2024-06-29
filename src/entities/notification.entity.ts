30a7b54e965b5e403c3eec5ea8879fe72b9c1bb5
won-n-only <siliver12@gmail.com>
2024-06-29 13:53:24 +0900

    update: 'notification.entity's 'from' type to string #52

--- src/entities/notification.entity.ts
+++ src/entities/notification.entity.ts
@@ -1,9 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Types } from 'mongoose'
 
 @Schema({ timestamps: true })
 export class Notification {
-  @Prop({ type: Types.ObjectId, required: true, ref: 'User' })
-  from: Types.ObjectId
+  @Prop({ type: String, required: true, ref: 'User' })
+  from: string
 }
 export const NotificationSchema = SchemaFactory.createForClass(Notification)
