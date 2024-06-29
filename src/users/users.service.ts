53bc86dedcb2d8854703ea16fd9d0c7f3a8aa401
won-n-only <siliver12@gmail.com>
2024-06-29 14:04:00 +0900

    update: REST API to handle nickname #52

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { UsersRepository } from './users.repository'
-import { ReqGetUserDto } from './dto/request/get-user.dto'
 import { ResGetUserDto } from './dto/response/get-user.dto'
 import { plainToClass } from 'class-transformer'
 
@@ -8,16 +7,16 @@ import { plainToClass } from 'class-transformer'
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async findOne(ReqGetUserDto: ReqGetUserDto): Promise<ResGetUserDto> {
-    const user = await this.usersRepository.findOne(ReqGetUserDto)
+  async findOne(nickname: string): Promise<ResGetUserDto> {
+    const user = await this.usersRepository.findOne(nickname)
     if (!user) throw new NotFoundException('유저가 없습니다.')
 
     const resGetUserDto = plainToClass(ResGetUserDto, user)
     return resGetUserDto
   }
 
-  async patchAvatar(filter: object, avatar: object): Promise<Object> {
-    const updatedUser = await this.usersRepository.updateAvatar(filter, avatar)
+  async patchAvatar(nickname: string, avatar: object): Promise<Object> {
+    const updatedUser = await this.usersRepository.updateAvatar(nickname, avatar)
     return updatedUser.avatar
   }
 }
