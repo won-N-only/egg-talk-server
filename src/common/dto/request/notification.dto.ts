b1c8b83d68cdb3a5bb32852785c91d28606ff3a5
won-n-only <siliver12@gmail.com>
2024-07-02 22:14:00 +0900

    update: add 'AcceptFriend.dto' #79

--- src/common/dto/request/notification.dto.ts
+++ src/common/dto/request/notification.dto.ts
@@ -1,4 +1,11 @@
+import { Types } from 'mongoose'
+
 export class AddFriendDto {
   userNickname: string
   friendNickname: string
+}
+export class AcceptFriend {
+  userNickname: string
+  friendNickname: string
+  notificationId: Types.ObjectId
 }
