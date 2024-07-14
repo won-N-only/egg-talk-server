c871359d42877539761e1ce98d3e9509367a6534
Amborsia <ghdrms1220@naver.com>
2024-07-14 21:27:26 +0900

    feat:filterQueue 대기열 어디있더라도 친구 검사할 수 있도록 수정

--- src/meeting/services/queue.service.ts
+++ src/meeting/services/queue.service.ts
@@ -1,59 +1,41 @@
 import { Injectable } from '@nestjs/common'
 import { Socket } from 'socket.io'
 import { MeetingService } from './meeting.service'
-import { CommonRepository } from '../../common/common.repository'
+import { CommonService } from '../../common/common.service'
+import * as NodeCache from 'node-cache'
 
 @Injectable()
 export class QueueService {
-  constructor(
-    private readonly meetingService: MeetingService,
-    private readonly commonRepository: CommonRepository,
-  ) {}
-
   private maleQueue: { name: string; socket: Socket }[] = []
   private femaleQueue: { name: string; socket: Socket }[] = []
+  private friendCache = new NodeCache({ stdTTL: 600 }) // 캐시 TTL 10분
+
+  constructor(
+    private readonly meetingService: MeetingService,
+    private readonly commonService: CommonService,
+  ) {}
 
   async addParticipant(name: string, socket: Socket, gender: string) {
-    const index = (
-      gender === 'MALE' ? this.maleQueue : this.femaleQueue
-    ).findIndex(p => p.name === name)
+    const queue = gender === 'MALE' ? this.maleQueue : this.femaleQueue
+    const index = queue.findIndex(p => p.name === name)
     if (index !== -1) {
-      // 기존 참가자를 제거하고 새로운 참가자로 덮어씁니다.
-      ;(gender === 'MALE' ? this.maleQueue : this.femaleQueue).splice(index, 1)
+      queue.splice(index, 1)
     }
-
-    // 새로운 참가자 추가
-    ;(gender === 'MALE' ? this.maleQueue : this.femaleQueue).push({
-      name,
-      socket,
-    })
+    queue.push({ name, socket })
     console.log(
       `${gender} Queue: `,
-      (gender === 'MALE' ? this.maleQueue : this.femaleQueue).map(p => p.name),
+      queue.map(p => p.name),
     )
+    await this.filterQueues()
   }
 
   removeParticipant(name: string, gender: string) {
-    switch (gender) {
-      case 'MALE':
-        this.maleQueue = this.maleQueue.filter(p => p.name !== name)
-        console.log(
-          'Update Male Queue : ',
-          this.maleQueue.map(p => p.name),
-        )
-        break
-
-      case 'FEMALE':
-        this.femaleQueue = this.femaleQueue.filter(p => p.name !== name)
-        console.log(
-          'Update Female Queue : ',
-          this.femaleQueue.map(p => p.name),
-        )
-        break
-      default:
-        console.error('성별 오류입니다.')
-        break
-    }
+    const queue = gender === 'MALE' ? this.maleQueue : this.femaleQueue
+    this[`${gender.toLowerCase()}Queue`] = queue.filter(p => p.name !== name)
+    console.log(
+      `Update ${gender} Queue: `,
+      this[`${gender.toLowerCase()}Queue`].map(p => p.name),
+    )
   }
 
   async findOrCreateNewSession(): Promise<string> {
@@ -63,7 +45,6 @@ export class QueueService {
     return newSessionId
   }
 
-  /* 남녀 3명씩 끊어서 처리하는 작업 */
   async handleJoinQueue(
     participantName: string,
     client: Socket,
@@ -73,80 +54,100 @@ export class QueueService {
     try {
       await this.addParticipant(participantName, client, gender)
 
-      // 큐 필터링 및 매칭 시도
       const result = await this.filterQueues()
       if (result) {
         const { sessionId, readyMales, readyFemales } = result
         return { sessionId, readyMales, readyFemales }
       }
 
-      const participants = this.meetingService.getParticipants(sessionId)
       console.log(
         'Current waiting participants: ',
-        participants.map(p => p.name),
+        this.meetingService.getParticipants(sessionId).map(p => p.name),
       )
       return { sessionId }
     } catch (error) {
       console.error('Error joining queue:', error)
-      await this.meetingService.deleteSession(sessionId)
+      if (sessionId) {
+        await this.meetingService.deleteSession(sessionId)
+      }
     }
   }
 
   async filterQueues() {
-    // 매칭 가능성 확인
     if (this.maleQueue.length >= 3 && this.femaleQueue.length >= 3) {
-      for (let i = 0; i < this.maleQueue.length; i++) {
-        const male = this.maleQueue[i]
-        const maleFriends = await this.commonRepository.getFriendNicknames(
-          male.name,
+      const maleCombinations = this.getCombinations(this.maleQueue, 3)
+      const femaleCombinations = this.getCombinations(this.femaleQueue, 3)
+
+      for (const males of maleCombinations) {
+        const maleFriends = await Promise.all(
+          males.map(m => this.getFriends(m.name)),
         )
-        const potentialFemales = this.femaleQueue.filter(
-          female => !maleFriends.includes(female.name),
-        )
+        const flatMaleFriends = maleFriends.flat()
 
-        if (potentialFemales.length >= 3) {
-          const readyMales = [male]
-          const readyFemales = potentialFemales.slice(0, 3)
-          const remainingMales = this.maleQueue.filter(
-            m => m.name !== male.name,
-          )
-          const remainingFemales = this.femaleQueue.filter(
-            f => !readyFemales.includes(f),
-          )
+        for (const females of femaleCombinations) {
+          const femaleNames = females.map(f => f.name)
 
-          // 남은 남성 큐에서 추가로 2명 선택
-          const additionalMales = remainingMales.slice(0, 2)
-          readyMales.push(...additionalMales)
-          this.maleQueue = remainingMales.slice(2)
-          this.femaleQueue = remainingFemales
+          if (flatMaleFriends.every(friend => !femaleNames.includes(friend))) {
+            const sessionId = await this.findOrCreateNewSession()
 
-          const sessionId = await this.findOrCreateNewSession()
+            await Promise.all([
+              ...males.map(male =>
+                this.meetingService.addParticipant(
+                  sessionId,
+                  male.name,
+                  male.socket,
+                ),
+              ),
+              ...females.map(female =>
+                this.meetingService.addParticipant(
+                  sessionId,
+                  female.name,
+                  female.socket,
+                ),
+              ),
+            ])
 
-          readyMales.forEach(male => {
-            this.meetingService.addParticipant(
-              sessionId,
-              male.name,
-              male.socket,
+            console.log('현재 큐 시작진입합니다 세션 이름은: ', sessionId)
+            await this.meetingService.startVideoChatSession(sessionId)
+
+            this.maleQueue = this.maleQueue.filter(
+              m => !males.map(rm => rm.name).includes(m.name),
             )
-          })
-
-          readyFemales.forEach(female => {
-            this.meetingService.addParticipant(
-              sessionId,
-              female.name,
-              female.socket,
+            this.femaleQueue = this.femaleQueue.filter(
+              f => !females.map(rf => rf.name).includes(f.name),
             )
-          })
 
-          console.log('현재 큐 시작진입합니다 세션 이름은 : ', sessionId)
-          await this.meetingService.startVideoChatSession(sessionId)
-
-          return { sessionId, readyMales, readyFemales }
-        } else {
-          return
+            return { sessionId, readyMales: males, readyFemales: females }
+          }
         }
       }
     }
     return null
+  }
+
+  private getCombinations(arr, size) {
+    const result = []
+    const f = (prefix, arr) => {
+      for (let i = 0; i < arr.length; i++) {
+        const newPrefix = prefix.concat(arr[i])
+        if (newPrefix.length === size) {
+          result.push(newPrefix)
+        } else {
+          f(newPrefix, arr.slice(i + 1))
+        }
+      }
+    }
+    f([], arr)
+    return result
+  }
+
+  private async getFriends(name: string): Promise<string[]> {
+    const cachedFriends = this.friendCache.get<string[]>(name)
+    if (cachedFriends) {
+      return cachedFriends
+    }
+    const friends = await this.commonService.sortFriend(name)
+    this.friendCache.set(name, friends)
+    return friends
   }
 }
